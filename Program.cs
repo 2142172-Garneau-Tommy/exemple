53af7544f4f2db0b93f27671ff3e17569236342c
Tommy <2142172@carrefour.cegepvicto.ca>
2022-02-06 20:35:14 -0500

    Commit finale

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace carré_cube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre = 4; 
+            
+            int nombres = 0;
+
+            Console.WriteLine("Entrez un nombre pour connaitre le carré de ce nombre");
+            int.TryParse(Console.ReadLine(), out nombre);
+            Console.WriteLine($"\nLe carré du nombre est {nombre  * nombre }",nombre );
+            Console.WriteLine("Entrez un autre nombre pour connaitre son cube");
+            int.TryParse(Console.ReadLine(), out nombres);
+            Console.WriteLine($"\nLe cube du nombre est {nombres * nombres * nombres}",nombres);
+           
+
+            
+            
+        }
+    }
+}
